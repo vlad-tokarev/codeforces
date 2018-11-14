ff67f1bb0471e098e10ad37406da1296580a4fde
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2018-11-14 15:16:45 +0300

    add big O estimation

--- solutions/981_B.py
+++ solutions/981_B.py
@@ -1,22 +1,30 @@
 from collections import defaultdict
 
+# computation complexity = n
+
 elements = defaultdict(int)
 
 if __name__ == '__main__':
 
     n = int(input())
-    for i in range(n):
+    for i in range(n):  # bigO = n
 
+        # if x3 - input
         a, x = [int(num) for num in input().split(' ')]
+
         if elements[a] < x:
             elements[a] = x
 
-    m = int(input())
+    # this loop the same as above =>  # bigO = n
+    m = int(input())  #
     for i in range(m):
 
         b, y = [int(num) for num in input().split(' ')]
         if elements[b] < y:
             elements[b] = y
 
+    # big(O) = n
     print(sum(elements.values()))
+
+    # => big(O) = n => 3n => n
 
